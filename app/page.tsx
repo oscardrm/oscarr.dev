bd3fd0da1352650db6d8d85beb4a3c0e6fb8f4a3
Oscar Román <oscar.roman@mundotelecomunicaciones.cl>
2023-05-13 00:24:10 -0400

    fix: bad reference of (use client) on layout file

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 'use client'
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import AvatarComponent from "./components/avatar/avatarComponent";
 import LinksSectionComponent from "./components/links-section/LinksSection";
 
 const Home = () => {
   return (
-    <>
-      <AvatarComponent />
-      <Flex align="center" justify="center">
-        <Text fontSize='4xl'>YOU CAN FIND ME HERE.</Text>
-      </Flex>
-      <LinksSectionComponent />
-    </>
+      <Box
+        maxW="960px"
+        mx="auto"
+        px={4}>
+        <AvatarComponent />
+        <Flex align="center" justify="center">
+          <Text fontSize='4xl'>YOU CAN FIND ME HERE.</Text>
+        </Flex>
+        <LinksSectionComponent />
+      </Box>
   )
 }
 export default Home;
